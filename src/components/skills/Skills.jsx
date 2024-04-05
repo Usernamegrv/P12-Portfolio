b9c073072407619c7e10ed0aad3765c16bdfa619
Usernamegrv <139330806+Usernamegrv@users.noreply.github.com>
2024-04-05 15:51:15 +0200

    skills refacto

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,14 +4,18 @@ function Skills() {
   return (
     <div className="container">
       <div className="logo-container">
-        <img src="./javascript.png" alt="logo javascript" />
-        <img src="./react.png" alt="logo react" />
-        <img src="./CSS3.png" alt="logo css3" />
-        <img src="./sass.png" alt="logo sass" />
-        <img src="./html5.png" alt="logo html" />
+        <img
+          src="./javascript.png"
+          alt="logo javascript"
+          className="logo-javascript"
+        />
+        <img src="./react.png" alt="logo react" className="logo-react" />
+        <img src="./CSS3.png" alt="logo css3" className="logo-css" />
+        <img src="./sass.png" alt="logo sass" className="logo-sass" />
+        <img src="./html5.png" alt="logo html" className="logo-html" />
       </div>
       <div className="bar back intermediate" data-skill="JAVASCRIPT"></div>
-      <div className="bar back intermediate" data-skill="REACT"></div>
+      <div className="bar back intermediate" data-skill="REACT-VITE"></div>
       <div className="bar front advanced" data-skill="CSS3"></div>
       <div className="bar front advanced" data-skill="SASS"></div>
       <div className="bar front expert" data-skill="HTML5"></div>
