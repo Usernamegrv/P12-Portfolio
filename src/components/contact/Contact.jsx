4d272f1c691f3a2596ed5784db848a94c66e271f
Usernamegrv <139330806+Usernamegrv@users.noreply.github.com>
2024-04-08 19:28:55 +0200

    contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import "./Contact.scss";
 function Contact() {
   return (
     <section id="contact">
-      <a href="mailto:morganegrvcontact@gmail.com">Un projet? Ecrivez-moi</a>
+      <a href="#">Un projet? Ecrivez-moi</a>
     </section>
   );
 }
